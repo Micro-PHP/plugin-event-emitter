64c3c4401bc15b6a85f8d47586f0cbf25b88a491
Komar Stanislau <head.trackingsoft@gmail.com>
2022-01-30 12:59:05 +0000

    Event emitter provider

---
+++ src/Business/Facade/EventsFacade.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Micro\Plugin\EventEmitter\Business\Facade;
+
+use Micro\Component\EventEmitter\EventEmitterFactoryInterface;
+use Micro\Component\EventEmitter\EventEmitterInterface;
+use Micro\Component\EventEmitter\EventInterface;
+use Micro\Component\EventEmitter\ListenerProviderInterface;
+use Micro\Plugin\EventEmitter\EventsFacadeInterface;
+
+class EventsFacade implements EventsFacadeInterface
+{
+    /**
+     * @var EventEmitterInterface
+     */
+    private EventEmitterInterface $eventEmitter;
+
+    /**
+     * @param EventEmitterFactoryInterface $eventEmitterFactory
+     */
+    public function __construct(
+        private EventEmitterFactoryInterface $eventEmitterFactory
+    )
+    {
+        $this->eventEmitter = $this->eventEmitterFactory->create();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function addProvider(ListenerProviderInterface $listenerProvider): void
+    {
+        $this->eventEmitter->addListenerProvider($listenerProvider);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function emit(EventInterface $event): void
+    {
+        $this->eventEmitter->emit($event);
+    }
+}
--- src/EventEmitterPlugin.php
+++ src/EventEmitterPlugin.php
@@ -3,80 +3,41 @@
 namespace Micro\Plugin\EventEmitter;
 
 use Micro\Component\DependencyInjection\Container;
-use Micro\Component\EventEmitter\EventEmitterBuilder;
+use Micro\Component\EventEmitter\EventEmitterFactory;
+use Micro\Component\EventEmitter\EventEmitterFactoryInterface;
 use Micro\Component\EventEmitter\EventEmitterInterface;
-use Micro\Component\EventEmitter\ListenerProviderFactory;
 use Micro\Component\EventEmitter\ListenerProviderInterface;
 use Micro\Framework\Kernel\Plugin\AbstractPlugin;
-use Closure;
+use Micro\Plugin\EventEmitter\Business\Facade\EventsFacade;
 
 class EventEmitterPlugin extends AbstractPlugin
 {
-    /**
-     * @var ListenerProviderInterface
-     */
-    private ?ListenerProviderInterface $listenerProvider = null;
-
     /**
      * {@inheritDoc}
      */
     public function provideDependencies(Container $container): void
     {
-        $container->register(EventEmitterInterface::class, $this->createEventEmitterServiceCallback());
-        $container->register(ListenerProviderInterface::class, $this->createEventListenerProviderServiceCallback());
+        $container->register(EventsFacadeInterface::class, function (Container $container) {
+            return $this->createFacade();
+        });
+
+ //       $container->register(EventEmitterInterface::class, $this->createEventEmitterServiceCallback());
+ //       $container->register(ListenerProviderInterface::class, $this->createEventListenerProviderServiceCallback());
     }
 
     /**
-     * @return ListenerProviderInterface
+     * @return EventsFacadeInterface
      */
-    private function createListenerProvider(): ListenerProviderInterface
+    protected function createFacade(): EventsFacadeInterface
     {
-        if(!$this->listenerProvider) {
-            $this->listenerProvider = (new ListenerProviderFactory())->create();
-        }
-
-        return $this->listenerProvider;
+        return new EventsFacade($this->createEventEmitterFactory());
     }
 
     /**
-     * @return EventEmitterBuilder
+     * @return EventEmitterFactoryInterface
      */
-    private function createEventEmitterBuilder(): EventEmitterBuilder
+    protected function createEventEmitterFactory(): EventEmitterFactoryInterface
     {
-        return new EventEmitterBuilder();
-    }
-
-    /**
-     * @return Closure
-     */
-    private function createEventEmitterServiceCallback(): Closure
-    {
-        return function(Container $container)
-        {
-            return $this->createEventEmitterService();
-        };
-    }
-
-    /**
-     * @return Closure
-     */
-    private function createEventListenerProviderServiceCallback(): Closure
-    {
-        return function(Container $container)
-        {
-            return $this->createListenerProvider();
-        };
-    }
-
-    /**
-     * @return EventEmitterInterface
-     */
-    private function createEventEmitterService(): EventEmitterInterface
-    {
-        $listenerProvider = $this->createListenerProvider();
-        $builder = $this->createEventEmitterBuilder();
-        $builder->addProvider($listenerProvider);
-
-        return $builder->build();
+        return new EventEmitterFactory();
     }
 }
